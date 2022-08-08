72000176a4039321c96f618ff13fe66d35a71b12
Jere Ljungberg <jere@mysteerio.fi>
2022-08-08 11:40:01 +0300

    Mock Drupal.t for tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,12 @@
 import { server } from './test/test-utils';
 
-beforeAll(() => server.listen());
+// Mock Drupal.t for tests.
+global.Drupal = {
+  t: (str, args, options) => str,
+};
+
+beforeAll(() => {
+  server.listen();
+});
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
