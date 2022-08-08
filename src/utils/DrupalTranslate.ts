ccf3ffd7cdacac57e95b8fdbb1292a20b2522817
Jere Ljungberg <jere@mysteerio.fi>
2022-08-08 11:25:51 +0300

    Remove unused file

--- src/utils/DrupalTranslate.ts
+++
@@ -1,9 +0,0 @@
-export const t = (str: string, args: Object = {}, options: Object = {}): string => {
-  if (Drupal) {
-    return Drupal.t(str, args, options);
-  }
-
-  return str;
-};
-
-export default t;
