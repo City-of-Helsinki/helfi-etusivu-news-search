60912131cbca09aba77370b71fc869293ff27a18
Jere Ljungberg <jere@mysteerio.fi>
2022-10-17 19:42:29 +0300

    Hide FormContainer from the app

--- src/containers/SearchContainer.tsx
+++ src/containers/SearchContainer.tsx
@@ -24,7 +24,7 @@ const SearchContainer = () => {
 
   return (
     <div>
-      <FormContainer initialState={getInitialParams()} />
+      {/* <FormContainer initialState={getInitialParams()} /> */}
       <ResultsContainer />
     </div>
   );
